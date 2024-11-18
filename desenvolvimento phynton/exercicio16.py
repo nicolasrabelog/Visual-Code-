1240f3657bb8906cbfac4024b891a397b978fd7c
nic <nicolasrabellmuniz@gmail.com>
2024-11-18 15:36:28 -0300

    exercicio16.py

---
+++ desenvolvimento phynton/exercicio16.py
@@ -0,0 +1,10 @@
+largura = float(input("informe a largura do terreno"))
+comprimento = float(input("informe o comprimento do terreno"))
+area = largura * comprimento
+if area < 100 :
+  print("Area do terreno e ", area, "m Terreno popular ")
+elif area >= 100 <= comprimento < 500:
+  print("Area do terreno e ", area, "m Terreno popular ")
+else :
+  print("Area do terreno e ", area, "m Terreno popular ")
+else:
