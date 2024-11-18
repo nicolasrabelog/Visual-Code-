6e344a275c00b323ac15f0a2fe3cf214c69e6c17
nic <nicolasrabellmuniz@gmail.com>
2024-11-18 17:17:40 -0300

    Delete desenvolvimento phynton/atividade12.py

--- desenvolvimento phynton/atividade12.py
+++
@@ -1,7 +0,0 @@
- ano = int(input("informe o ano atual "))
-nascimento = int(input("informe o ano do nascimento "))
-idade = ano - nascimento
-if idade < 18 :
-    print("Faltam",18-idade, " anos para você fazer o alistamento")
-else :
-    print("Você ja tem", idade-18, "anos que fez o alistamento")
