e4761124efe38b8d7d2219a9a0361d3e23bd39d9
nic <nicolasrabellmuniz@gmail.com>
2024-11-19 15:32:25 -0300

    Update and rename exercicio8.py to exercicio7.py

---
+++ desenvolvimento phynton/exercicio7.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite outra nota"))
+n2 = float(input("Digite outra nota"))
+media = (n1+n2)/2 
+if media >= 6 :
+    print("Aprovado media, ",media)
+else :
+    print("Reprovado media, ",media)
--- desenvolvimento phynton/exercicio8.py
+++
@@ -1,5 +0,0 @@
-velocidade= float(input("informe a velocidade "))
-if velocidade > 80 :
-    multa = (velocidade - 80) * 5
-    print(f"Velocidade acima de 80km - Multa R${multa:.2f}")
-  
