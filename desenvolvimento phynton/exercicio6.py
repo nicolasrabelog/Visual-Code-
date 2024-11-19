8cd8cef4107656e985323d046e642c2b334d026e
nic <nicolasrabellmuniz@gmail.com>
2024-11-19 15:31:34 -0300

    Update and rename exercicio7.py to exercicio6.py

---
+++ desenvolvimento phynton/exercicio6.py
@@ -0,0 +1,14 @@
+#Crie um algoritmo que leia um número real e mostre a tela o seu dobro e a 
+#sua terça parte.
+#Ex:
+#Digite um número: 3.5
+#o dobro de 3.5 é 7.0
+#A terça parte de 3.5 é 1.16666
+num = float(input("Digite um numero"))
+dobro = num * 2
+metade = num /2
+terco = num /3
+print("o dobro de ", num, "e",dobro)
+print("o metade de ", num, "e", metade)
+print("A terça parte", num, "e", terco).
+
