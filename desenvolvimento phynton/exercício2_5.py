75603d049b52e796e9ac5dbc5272471de7960f37
nic <nicolasrabellmuniz@gmail.com>
2024-11-19 15:53:52 -0300

    Create exercício2_5.py

---
+++ desenvolvimento phynton/exercício2_5.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um numero"))
+n2 = int(input("Digite outro numero"))
+
+if n1 > n2 :
+    print("O primeiro valor é o maior")
+elif n2 > n1 :
+    print("O segundo valor é o maior")
+else :
+    print("Não existe valor maior, os dois são iguais")
