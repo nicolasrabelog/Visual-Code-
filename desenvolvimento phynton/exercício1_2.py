0f5646eaea341f898b6c9095d723fd98ec17c451
nic <nicolasrabellmuniz@gmail.com>
2024-11-19 15:41:33 -0300

    Create exercício1_2.py

---
+++ desenvolvimento phynton/exercício1_2.py
@@ -0,0 +1,6 @@
+preco promocional
+preco = float(input("informe o preco o produto"))
+desconto = preco * 0.05
+promocao = preco - desconto
+print("Desconto", desconto)
+print("Preco promocional ", promocao )
