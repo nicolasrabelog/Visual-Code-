84a6d495cc65479b42e9a6260bdddda2482f40be
nic <nicolasrabellmuniz@gmail.com>
2024-11-19 15:41:06 -0300

    Update exercício1_1.py

--- desenvolvimento phynton/exercício1_1.py
+++ desenvolvimento phynton/exercício1_1.py
@@ -1,1 +1,6 @@
+a = float(input("informe o valor de A: "))
+b = float(input("informe o valor de B: "))
+c = float(input("informe o valor de C: "))
+delta = b**2 - 4 * a * c
+print("O valor de Delta = ", delta)
 
