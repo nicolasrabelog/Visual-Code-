870d0418bd73808b345a17835e4ecfe09d2c2145
nic <nicolasrabellmuniz@gmail.com>
2024-11-22 16:41:24 -0300

    exercicio43.py

---
+++ desenvolvimento phynton/exercicio43.py
@@ -0,0 +1,6 @@
+contador = 30
+while (contador >=0):
+    if contador % 4 ==0:
+        print(f["{contador}]")
+        print(contador)  # Exibe os números divisíveis por 4
+    contador -= 1  # Decrementa o contador a cada iteração
