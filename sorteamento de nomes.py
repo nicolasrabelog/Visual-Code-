4c5e08a651d86aa9d38155f3bf6e46a46709ba43
nic <nicolasrabellmuniz@gmail.com>
2024-12-03 15:14:54 -0300

    sorteamento de nomes.py

---
+++ sorteamento de nomes.py
@@ -0,0 +1,10 @@
+import random
+
+#Definindo o nome dos alunos da turma
+alunos = ["Samuel", "Isabella", "Lara", "Luciel", "Tiago"]
+
+#Sortear um aluno da lista
+aluno_sorteado = random.choice(alunos)
+
+#Exibindo o nome do aluno aluno sorteado 
+print(f'O aluno sorteado foi:{aluno_sorteado}')
